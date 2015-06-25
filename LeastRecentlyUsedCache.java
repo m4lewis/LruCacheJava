f6eefeb4f6839a2e7d58b4db2eff54ec8ffc3caf
Matt Lewis <mlewis@tangamgaming.com>
2015-06-25 19:46:47 -0400

    Took main method out of class

--- LeastRecentlyUsedCache.java
+++ LeastRecentlyUsedCache.java
@@ -123,32 +123,6 @@ public class LeastRecentlyUsedCache
             tail = head;
 
     }
-
-    public static void main(String[] args) {
-
-        LeastRecentlyUsedCache lr = new LeastRecentlyUsedCache(5);
-        System.out.println(lr.toString());
-        lr.put(1, 1);
-        lr.put(2, 2);
-        System.out.println("Cache currently has room for " + lr.getMaxSize() + " more item(s)");
-        System.out.println(lr.toString());
-        lr.put(3, 3);
-        lr.put(4, 4);
-        lr.put(5, 5);
-        System.out.println("Cache currently has room for " + lr.getMaxSize() + " more item(s)");
-        System.out.println(lr.toString());
-        Object val = lr.get(1);
-        System.out.println("" + val);
-        lr.put(6, 6);
-        System.out.println("Cache currently has room for " + lr.getMaxSize() + " more item(s)");
-        System.out.println(lr.toString());
-        Object val2 = lr.get(2);
-
-        if(val2 == null) System.out.println("Value Not Found");
-        else System.out.println("Something went wrong");
-
-    }
-
 }
 
 
